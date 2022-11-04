519fa322a4915d6d66f5634877e0309866c7dd7b
PedroVargasPallini <114812887+PedroVargasPallini@users.noreply.github.com>
2022-11-04 17:49:24 +0000

    Add files via upload

---
+++ average.cc
@@ -0,0 +1,11 @@
+#include <iostream> 
+using namespace std;
+
+int main () {
+	double input1;
+	double input2;
+	double input3;
+	cin >> input1 >> input2 >> input3;
+	cout << "Your average is: " << ( input1 + input2 + input3 ) / 3;
+	cout << endl;
+}
---
+++ sum.cc
@@ -0,0 +1,12 @@
+#include <iostream> 
+using namespace std;
+
+int main () {
+	int number1;
+	int number2; 
+	int number3;
+	cout << "Write three numbers:" << endl;
+	cin >> number1 >> number2 >> number3;
+	cout << "The sum of your numbers is: " << number1 + number2 + number3;
+	cout << endl;
+}
